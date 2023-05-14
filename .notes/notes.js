be1bef55021d663b5e0e3e010f705709227b7bcb
tnadasen <tnadasen@outlook.com>
2023-05-14 09:07:07 +0200

    First commit

---
+++ .notes/notes.js
@@ -0,0 +1,46 @@
+/*
+
+        Fullstack React Course (2022)
+        https://www.youtube.com/playlist?list=PL1GlgOJ9R3jgBuLQtSXrEs70iJbl3Ch49
+        https://github.com/MattDobsonWeb/react-fullstack-tutorial
+
+        npm init --yes
+        npm install express dotenv
+        npm install --save-dev nodemon concurrently
+
+        npm run server
+
+        --stop server
+
+        git init
+
+        --create the repo on GitHub
+
+        https://github.com/tnadasen/mern-react-mtd
+
+        --push an existing repository from the command line
+
+        git remote add origin https://github.com/tnadasen/mern-react-mtd.git
+
+        --add changes to a commit
+
+        git add .
+
+        --commit file changes
+
+        git -m "First commit"
+
+        git push origin main
+        
+
+
+
+
+
+
+
+
+
+
+
+*/
